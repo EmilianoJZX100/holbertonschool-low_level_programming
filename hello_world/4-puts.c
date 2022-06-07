0d09200bc02a28e1d11b62d60550428c9b653e75
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-07 09:41:49 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #!/bin/bash
-
 #include<stdio.h>
+
+/**
+ * main - we can pass parameters to the main() function
+ * Return - terminates the execution of a function and returns control to the calling function
+ **/
+
 int main()
 {
 	char Mystr1 = "\"Programming is like building a multilingual puzzle";
