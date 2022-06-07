63007c599383d3bd3876dd5ac4ec917c41f6bbbf
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-07 09:56:38 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include<stdio.h>
 
 /**
- * main - we can pass parameters to the main() function
- * Return - terminates the execution of a function and returns control to the calling function
- **/
+* main - entry point
+* Return: terminates the execution of a function
+*/
 
-int main()
+int main(void)
 {
-	char Mystr1 = "\"Programming is like building a multilingual puzzle";
-
-       puts("\"Programming is like building a multilingual puzzle");
-
-       return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
