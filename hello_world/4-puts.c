1ce83703f09bd54cd04d318a496702a766716c03
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-07 08:04:31 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,6 @@
 #include <stdio.h> // tells the preprocessor to include the contents of the standard input/output header (<stdio.h>) in the program
 
 int main()
-
 {
        puts( "\"Programming is like building a multilingual puzzle" )
 }
