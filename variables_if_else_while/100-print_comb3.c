6aa4c0713062dcb633a20ca50f99bb0de244d9ad
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-08 07:29:43 -0700

    100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: 0
+*/
+
+int main(void)
+{
+	int n, m;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		for (m = n ; m <= '9' ; m++)
+	{
+		if (n != m)
+		putchar(n);
+                putchar(m);
+		if (n != '8' || m != '9');
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+		putchar('\n');
+	return (0);
+}
