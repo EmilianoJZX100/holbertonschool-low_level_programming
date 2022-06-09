760709bf34a8a9129078ae97e66dfbf166cc2a97
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-08 17:05:26 -0700

    100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
 * main - entry point
 * Return: 0
 */
-
 int main(void)
 {
 	int n, m;
@@ -12,17 +10,17 @@ int main(void)
 	for (n = '0' ; n <= '9' ; n++)
 	{
 		for (m = n ; m <= '9' ; m++)
-	{
-		if (n != m)
-		putchar(n);
-		putchar(m);
-		if (n != '8' || m != '9')
 		{
-			putchar(',');
-			putchar(' ');
+			if (n != m)
+				putchar(n);
+			putchar(m);
+			if (n != '8' || m != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-	}
-		putchar('\n');
+	putchar('\n');
 	return (0);
 }
