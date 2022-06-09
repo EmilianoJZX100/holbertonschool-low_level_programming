7ce5e470319acc8d5d7baae1a0045e70bed19032
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-09 11:43:05 -0700

    print alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - execute
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 1 ; a < 11 ; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar ('\n');
+	}
+}
