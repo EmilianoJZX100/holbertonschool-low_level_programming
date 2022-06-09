90c31e045b5a87bdc4750537e1714ead60e28143
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-09 10:12:41 -0700

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet - in lower case
+*
+*Return: 0
+*/
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		_putchar (a);
+	}
+	_putchar ('\n');
+}
