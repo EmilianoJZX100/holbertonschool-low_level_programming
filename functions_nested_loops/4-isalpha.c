afa2952afe957cb10ac512e754c19d8c8ce78fa9
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-10 07:41:53 -0700

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - print function
+ * @c: char
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
+
