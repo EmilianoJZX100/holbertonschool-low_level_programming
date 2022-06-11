717b40e1c6635f2556da9d8202b67bc228ee3e86
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-11 12:58:11 -0700

    main

--- debugging/0-main.c
+++ debugging/0-main.c
@@ -1,19 +1,30 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - entry point
+* Return: 0
+*/
+
 int main(void)
 {
-		int n;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
