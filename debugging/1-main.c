ddc02c7f0558285e4c2558ebbb983743346db780
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-11 15:10:04 -0700

    1-main.c

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n"); //here the loop start//
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;// //
+	i = 0;
 
-        while (i < 10)// //
-        {
-                putchar(i);// //
-        }
+	/*while (i < 10)*/
+	{
+	putchar(i);
+	}
 
-        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
 }
