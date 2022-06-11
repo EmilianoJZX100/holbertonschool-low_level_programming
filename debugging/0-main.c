f3eedf6fe1046f3b1087a99ac427dd3783bba592
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-11 13:09:23 -0700

    main

--- debugging/0-main.c
+++ debugging/0-main.c
@@ -3,28 +3,19 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main - entry point
-* Return: 0
+* _positive_or_negative - entry point
+* * Return: 0
 */
-
-int main(void)
+int _positive_or_negative(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
 	}
 	return (0);
 }
