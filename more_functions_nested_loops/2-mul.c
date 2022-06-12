f0404a8189cb31e578ec0730ce453e919dd02712
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-12 10:25:13 -0700

    multiplier

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ * Return: multiplier result
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
