b6caaecc8fd770965c9405f597528c3ec368fabc
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-13 16:02:47 -0700

    print triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - function
+ * @size: table
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int j, i;
+
+	for (i = 0; i < size; i++)
+		{
+			for (j = 1; j < size; j++)
+				{
+					if ((i - j) >= size)
+						{
+							_putchar('#');
+						}
+					else
+					{
+						_putchar(' ');
+						}
+					}
+			}
+}
