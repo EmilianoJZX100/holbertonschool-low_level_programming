043cafe58cfe70aa5a974c958eebd60e68295ccd
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-14 13:33:58 -0700

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the leng of a string
+ * Return: Var
+ */
+int _strlen(char *s)
+{
+	int i, a = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		a = a + 1;
+	}
+		return (a);
+}
---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * Return: 0
+ */
+void _puts(char *str);
+{
+
