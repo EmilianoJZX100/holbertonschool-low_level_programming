751d69ca20bcb5d92e0fc99fb88e780bf51e54e1
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-14 15:02:06 -0700

    print reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; --i)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
