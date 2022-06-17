bc20e88be1d29fc7def9527a6bac260b4b908a28
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-17 11:08:53 -0700

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,15 @@ char *_strcat(char *dest, char *src)
 {
 	int d;
 	int s;
+	int e;
 
 	for (d = 0; dest[d] != '\0'; d++)
 		;
 	for (s = 0; src[s] != '\0'; s++)
 		;
+	for (e = 0; src[e] <= s; e++)
 	{
-		dest[d] = src[s];
+		dest[d] = src[e];
 		d++;
 	}
 	return (dest);
