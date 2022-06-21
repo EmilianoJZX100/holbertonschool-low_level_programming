63fc903604b4b75ec326861143d820c6d9d04528
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-21 15:23:51 -0700

    prime

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * primo - find the prime number
+ * @m: var
+ * @n: prime number
+ * Return: primo
+ */
+int primo(int n, int m)
+{
+	if (m == 1)
+	{
+		return (1);
+	}
+	else if ((n % m) == 0)
+	{
+		return (0);
+	}
+	else
+		return (primo(n, m -1));
+}
+/**
+ * is_prime_number - function
+ * @n:number
+ * Return: primo
+ */
+int is_prime_number(int n)
+{
+	if (n > 1)
+	{
+		return (primo(n, n - 1));
+	}
+	else
+	{
+	return (0);
+	}
+}
--- recursion/main.h
+++ recursion/main.h
@@ -9,4 +9,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int helper(int n, int m);
+int is_prime_number(int n);
+int primo(int n, int m);
 #endif
