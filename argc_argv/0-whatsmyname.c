e6e490ee4194b628cf7d69d55f2dff6fbd9dd9f7
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-21 16:42:17 -0700

    mynameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program that print its name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s", argv[0]);
+		return (0);
+}
