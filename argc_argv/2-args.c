4a7ca09a79b26bc79f6d14b40225c7d943293f57
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-21 17:48:24 -0700

    2 args

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,13 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - prints all arguments it receives
  * @argc: nombre
  * @argv: argumentos
+ * @a: int
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+	printf("%s\n", argv[a]);
+	}
 	return (0);
 }
