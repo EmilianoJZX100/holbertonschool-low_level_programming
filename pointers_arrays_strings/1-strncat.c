580bb2e1dfe847071c2a345ea4f2492f71868003
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-22 07:16:47 -0700

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -3,6 +3,7 @@
  * _strncat - cat 2 strings w/o the \0 char
  * @dest: destino
  * @src: origen
+ * @n: integer
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
@@ -10,9 +11,9 @@ char *_strncat(char *dest, char *src, int n)
 	int d, s, e;
 
 	for (d = 0; dest[d] != '\0'; d++)
-		;
+	{}
 	for (s = 0; src[s] != n; s++)
-		;
+	{}
 	for (e = 0; e <= s; e++)
 	{
 		dest[d] = src[e];
