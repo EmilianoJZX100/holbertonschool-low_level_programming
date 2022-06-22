70ec89a655ec507a48384e9ae31295c51549db02
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-21 17:35:07 -0700

    3 args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints all arguments it receives
+ * @argc: nombre
+ * @argv: argumentos
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
