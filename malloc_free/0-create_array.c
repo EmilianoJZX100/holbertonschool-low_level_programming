496cd9af68205b4801617e785da4e7a35e1b6737
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-23 09:27:22 -0700

    array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *create_array - creates an array of chars
+ * @size: largo del array
+ * @c: caracter a imprimir
+ * Return: pr
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *pr;
+	unsigned int i;
+
+	pr = malloc(size * sizeof(char));
+
+	for (i = 0; i < size ; i++)
+	{
+		pr[i] = c;
+	}
+	if (size == 0)
+		return (NULL);
+	else
+		return (pr);
+	if (pr == NULL)
+		return (0);
+	return (pr);
+}
