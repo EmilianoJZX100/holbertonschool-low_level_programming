5e39236cda541fae91279dc9c160d248a6c0cf72
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-23 16:31:25 -0700

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,44 +2,39 @@
 #include <string.h>
 /**
  * *str_concat - function that concatenates two strings
- * @i: lenght of s3
- * @j: lenght of s1
- * @k: lenght of s2
+ * @s1: iterador de size1
+ * @s2: iterador de size2
  * Return: s3
  */
 char *str_concat(char *s1, char *s2)
 {
 	int i = 0;
 	int j = 0;
-	int k = 0;
 	char *s3;
 	int size1 = strlen(s1);
 	int size2 = strlen(s2);
-	int sizet = (size1 + size2) + 1;
-
+		
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		size1 = 0;
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		size2 = 0;
 	}
-	s3 = malloc((size1 + size2) * sizeof(char) + 1);
-
-	for (i = 0; i < sizet;)
+	size1 = strlen(s1);
+	size2 = strlen(s2);
+	s3 = malloc((size1 + size2) + 1);
+	{
+		for (i = 0; i <= size1;i++)
 		{
-			for (j = 0; j < size1; j++)
-			{
-				s3[i] = s1[i];
-				i++;
-			}
-			for (k = 0; k < size2; k++)
-			{
-				s3[i] = s2[i];
-				i++;
-			}
+			s3[i] = s1[i];
 		}
+		for (j = 0; j <= size2; j++)
+		{
+			s3[i] = s2[j];
+			i++;
+		}
+	}
 	return (s3);
 }
-
