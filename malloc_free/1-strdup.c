5188ed0dd188fd1f85744def9911fc5cd609f52d
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-23 10:44:20 -0700

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory
+ * @str: string a copiar
+ * Return: pr
+ */
+char *_strdup(char *str)
+{
+	char *pr;
+	int i;
+	int size;
+
+	size = strlen(str);
+	pr = malloc(size * sizeof(char));
+	if (pr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+
+		pr[i] = str[i];
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	return (pr);
+}
