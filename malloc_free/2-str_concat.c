8ece84e69af056a7a31501b71c35db3b0759fad1
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-23 17:02:16 -0700

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,8 +8,8 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 	char *s3;
 	int size1 = strlen(s1);
 	int size2 = strlen(s2);
@@ -25,16 +25,14 @@ char *str_concat(char *s1, char *s2)
 	size1 = strlen(s1);
 	size2 = strlen(s2);
 	s3 = malloc((size1 + size2) + 1);
-	{
-		for (i = 0; i <= size1;i++)
+		for (i = 0; s1[i]; i++)
 		{
 			s3[i] = s1[i];
 		}
-		for (j = 0; j <= size2; j++)
+		for (j = 0; s2[j]; j++)
 		{
-			s3[i] = s2[j];
-			i++;
+			s3[i + j] = s2[j];
 		}
-	}
-	return (s3);
+		s3[i + j] = '\0';
+		return (s3);
 }
