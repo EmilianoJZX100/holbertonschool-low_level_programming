b8d49f50d8492437608ccc61e7facae8853635dc
EmilianoJZX100 <5083@holbertonschool.com>
2022-06-28 15:00:15 -0700

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,10 @@
-#ifndef MAIN_H
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - new type of struct
+ * @name: what the dog is called
+ * @age: years of the dog
+ * @owner: who the dog belongs to
  */
 struct dog
 {
