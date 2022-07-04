67617359c557e2a9db8acaeeff97aacc664f594c
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-03 19:19:00 -0700

    strings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			}
 			else
 			{
-				printf("%s%s\n", separator, str);
+				printf("%s%s", separator, str);
 			}
 	}
 	va_end(list);
