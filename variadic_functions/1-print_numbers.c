71f80bbb9ec1cd17e49a0322af9d66ffac563bdc
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-03 18:08:38 -0700

    print

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,11 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 		pri = va_arg(list, int);
 
-	if (separator == NULL)
-	{
-		return;
-	}
-	else if (separator != NULL)
+	if (separator != NULL)
 	{
 		printf("%d%s", pri, separator);
 	}
