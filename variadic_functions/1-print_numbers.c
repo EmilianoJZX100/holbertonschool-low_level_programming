65f05ef1dc1524d3b5ae3fd712ce9f557723b31a
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-03 18:16:34 -0700

    print

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(list, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		pri = va_arg(list, int);
 
