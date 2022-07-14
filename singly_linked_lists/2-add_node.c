d2fc513bb3456909cb7b8793e6d668d5f79ab331
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-14 16:54:22 -0700

    a

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_node - adds a node at the beggining
+ * Return: newnode
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	newnode = malloc(sizeof(list_t));
+		if (newnode == NULL)
+		{
+			return (NULL);
+		}
+	newnode->next = *head;
+	newnode->len = strlen(str);
+	newnode->str = strdup(str);
+	*head = newnode;
+	
+	return (newnode);
+}
