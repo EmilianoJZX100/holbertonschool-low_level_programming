844947e253f08543be32cf9a41533bb751dde9a7
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-14 18:02:13 -0700

    a

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_node_end - adds a new node at the end of a list
+ *
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	newnode = malloc(sizeof(list_t));
+		if (newnode == NULL)
+		{
+			return (NULL);
+		}
+		if (*head == NULL)
+		{
+			*head = newnode;
+		}
+	newnode->next = NULL;
+	newnode->len = strlen(str);
+	newnode->str = strdup(str);
+
+	return (newnode);
+}
