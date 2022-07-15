92f84a54de9509fc7da66dd14e7d891bedde7142
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-14 19:02:44 -0700

    a

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ */
+void free_list(list_t *head)
+{
+	list_t *frees;
+
+	while ((frees = head))
+	{
+		if (frees->str)
+			free(frees->str);
+		head = head->next;
+		free(frees);
+	}
+}
