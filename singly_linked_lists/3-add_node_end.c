1004ecf25c07791fbeed355cd5e40da5b6e49a7a
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-14 18:37:40 -0700

    a

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,19 +10,21 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *newnode, *end = *head;
 
 	newnode = malloc(sizeof(list_t));
-		if (newnode)
+		if (!newnode)
+			return (NULL);
 		{
 			if (*head == NULL)
 			{
 				*head = newnode;
+				return (newnode);
 			}
 			else
 			{
 				while (end->next != NULL)
 				{
 					end = end->next;
-					end->next = newnode;
 				}
+				end->next = newnode;
 			}
 		newnode->next = NULL;
 		newnode->len = strlen(str);
