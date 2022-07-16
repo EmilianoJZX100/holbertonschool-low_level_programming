f61dc37da733b77c1f8c54c1770783691c129f30
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-15 17:05:30 -0700

    free list

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *frees;
 
+	if (!head)
+	{
+
 		while (*head)
 	{
 		frees = (*head);
 		*head = (*head)->next;
 		free(frees);
+	}
 	}
 		head = NULL;
 }
