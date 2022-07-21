83a67da306d374bd57fd59deb1a336d3ef3e5ca0
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-20 18:23:39 -0700

    read text

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX std output
+ * Return: actual numbers of letters, 0 if it fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int contador, texto;
+	char *archivo;
+
+	if (!filename)
+		return (0);
+
+	archivo = malloc(sizeof(char) * letters);
+	if (!archivo)
+		return (0);
+
+	texto = open(filename, O_RDONLY);
+	if (texto == -1)
+		return (0);
+
+	contador = read(texto, archivo, letters);
+
+	write(STDOUT_FILENO, archivo, contador);
+	close(texto);
+	return (contador);
+}
