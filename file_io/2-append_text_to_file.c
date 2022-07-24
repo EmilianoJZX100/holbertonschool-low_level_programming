5bc0fcdb184252633de61dd1c1e61ad4f0d25b26
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-24 15:19:36 -0700

    append text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,6 +14,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_RDWR | O_APPEND);
 
+	if (fd != -1)
+		return (-1);
+
 	if (!text_content)
 	{
 		if (fd != -1)
