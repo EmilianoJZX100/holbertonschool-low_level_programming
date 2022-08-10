b06fdc1d0953eb254cf1b8b527ff547a1007e4e7
EmilianoJZX100 <5083@holbertonschool.com>
2022-08-10 06:51:21 -0700

    repechaje

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -7,6 +7,8 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	int i;
+
 	op_t ops[] = {
 	{"+", op_add},
 	{"-", op_sub},
@@ -15,9 +17,8 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-	int i;
 
-    	while (ops[i].op)
+	while (ops[i].op)
 	{
 		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
