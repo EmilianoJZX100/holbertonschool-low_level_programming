218f917a928ec8287d0bab7714bef11677750430
EmilianoJZX100 <5083@holbertonschool.com>
2022-08-10 17:22:15 -0700

    repechaje

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* reverse_array - reverse string
+* @a: array
+* @n: array length
+*/
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j;
+
+	for (n = n - 1; i < n; i++, n--)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
