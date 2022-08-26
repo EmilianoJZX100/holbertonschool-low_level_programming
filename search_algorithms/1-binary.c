0134f8d5cb311a41261318d90eda4a2ca2e18378
EmilianoJZX100 <5083@holbertonschool.com>
2022-08-26 12:30:14 -0700

    time & space complexity

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,20 @@
+#include "search_algos.h"
+/**
+ * binary_search - search a value in a sorted array of int using binary search
+ * @array: pointer to first element of the array to search in
+ * @size: number of elements of the array
+ * @value: value to search for
+ * Return: int
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (!array || size == 0)
+		return (-1);
+	
+	for (i = 0; i < n; i++)
+
+
+}
+
+
+	
