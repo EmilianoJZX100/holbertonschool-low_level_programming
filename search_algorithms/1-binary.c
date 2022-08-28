6ffc54ef1d9dc742753284235130b8239b161799
EmilianoJZX100 <5083@holbertonschool.com>
2022-08-28 09:05:37 -0700

    binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -8,13 +8,30 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (!array || size == 0)
+	size_t i, left, right;
+	
+	if (!array)
 		return (-1);
-	
-	for (i = 0; i < n; i++)
 
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
 
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+
+		if (array[i] == value)
+			return (i);
+
+		if (array[i] > value)
+			right = i - 1;
+
+		else
+			left = i + 1;
+	}
+	return (-1);
 }
-
-
-	
